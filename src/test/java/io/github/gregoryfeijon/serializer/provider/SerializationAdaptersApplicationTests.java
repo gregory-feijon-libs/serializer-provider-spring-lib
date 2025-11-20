d44f3913a2ca548f6066f56f024c638145fbea08
Gregory Feijon <gregory.feijon@outlook.com>
2025-11-20 01:09:12 -0300

    [CREATE] Initial Commit

---
+++ src/test/java/io/github/gregoryfeijon/serializer/provider/SerializationAdaptersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gregoryfeijon.serializer.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializationAdaptersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
